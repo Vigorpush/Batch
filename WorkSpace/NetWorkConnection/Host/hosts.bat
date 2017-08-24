ded0005ab3cb41b4624b7d108d80717e2761ac31
Zang JiaWei --- Vigor <vigorzjw@gmail.com>
2017-08-24 01:25:17 -0400

    typos fixed and batch file fixed

---
+++ WorkSpace/NetWorkConnection/Host/hosts.bat
@@ -0,0 +1,15 @@
+@color a0
+@echo off
+color f2
+
+echo.
+
+copy "hosts" "C:\WINDOWS\system32\drivers\etc\hosts"
+echo.
+echo.=====================================================================
+echo.Quick Setting Finished£¡
+echo. & pause 
+cls
+
+echo.
+pause
